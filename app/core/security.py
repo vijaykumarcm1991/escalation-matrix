911d28c63ac6c353c9016b944e6d175e29e520ec
vijaykumarcm1991 <vijaykumarcm20@gmail.com>
2026-02-19 13:07:17 +0530

    Security Hardening Phase 1

--- app/core/security.py
+++ app/core/security.py
@@ -2,10 +2,11 @@ from datetime import datetime, timedelta
 from jose import jwt, JWTError
 from passlib.context import CryptContext
 import os
+from app.core.config import settings
 
-SECRET_KEY = "supersecretkey"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
+SECRET_KEY = settings.SECRET_KEY
+ALGORITHM = settings.ALGORITHM
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.ACCESS_TOKEN_EXPIRE_MINUTES
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
